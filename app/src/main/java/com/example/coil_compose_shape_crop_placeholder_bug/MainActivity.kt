7f9e268eb542cb07a4079b082bc8dd22f9a6db3c
Tiago Nunes <tiago.nunes.lx@gmail.com>
2021-08-10 21:52:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/coil_compose_shape_crop_placeholder_bug/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.coil_compose_shape_crop_placeholder_bug
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.coil_compose_shape_crop_placeholder_bug.ui.theme.CoilcomposeshapecropplaceholderbugTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoilcomposeshapecropplaceholderbugTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun App() {
+    val urls = listOf(
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+        "https://images.unsplash.com/photo-1607330289024-1535c6b4e1c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
+    )
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+    ) {
+        items(urls) { url ->
+            Surface(
+                shape = RoundedCornerShape(percent = 20),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.6f)
+                    .aspectRatio(1f),
+            ) {
+                Image(
+                    painter = rememberImagePainter(
+                        data = url,
+                        builder = {
+//                            placeholder(R.drawable.ic_food)
+//                            crossfade(true)
+                        }
+                    ),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CoilcomposeshapecropplaceholderbugTheme {
+        App()
+    }
+}
